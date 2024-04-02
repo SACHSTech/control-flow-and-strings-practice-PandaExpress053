1f3921697fbf33fbd0a192f3c3f29602e82265e0
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-04-02 13:51:38 -0400

    modified:   C4IsSandwich.java

--- C4IsSandwich.java
+++ C4IsSandwich.java
@@ -1,1 +1,29 @@
 
+class C4IsSandwich extends ConsoleProgram {
+
+    /**
+    * Description A sandwich is two pieces of bread with something in between. 
+    * Return the string that is between the first and last appearance of "bread" 
+    * in the given string, or output the empty string "" if there are not two 
+    * pieces of bread.
+    * @author: E. Fung
+    */
+    
+    public void run() {
+      int intFirstBread, intSecondBread;
+      String stringInput = readLine((""));
+
+      intFirstBread = stringInput.indexOf("bread");
+      if (intFirstBread != -1){
+        intSecondBread = stringInput.indexOf("bread", intFirstBread + 5);
+        if (intSecondBread != -1){
+            String result = stringInput.substring(intFirstBread + 5, intSecondBread);
+            System.out.println(result);
+        }
+        else{}
+      }
+      else{}
+      
+    }
+  }
+  
